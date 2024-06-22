750510092086772714bd3653219ffd38f0397254
64-HiroByte <higher.ground.rc@gmail.com>
2024-06-22 14:44:39 +0900

    controller.pyにSuicaのチャージ処理を追加

--- python/vending_machine/controller.py
+++ python/vending_machine/controller.py
@@ -3,19 +3,25 @@ from models import Suica
 from models import VendingMachine
 
 
-def generate_suica(deposit=500):
-    suica = Suica(deposit)
-    return suica
 
 def charge_to_suica(amount):
     if amount >= 100:
-        suica.deposit = amount
+        suica.deposit = amount  # 正の値は残高チャージ
     else:
         print('[exeption] 100円未満のチャージはできません')
 
+'**************************************************************'
 
-suica = generate_suica()
 
-charge_to_suica(900)
+
+
+DEFAULT_DEPOSIT = 500
+PRICE_DICT = {'ペプシ': 150, 'いろはす': 120, 'モンスター': 230}
+
+suica = Suica(DEFAULT_DEPOSIT)
+
+# チャージ処理
+input_amount = int(input('チャージする金額を入力してください（最低チャージ額: 100円） > '))
+charge_to_suica(input_amount)
 print(f'Suicaの残高: {suica.deposit}円')
 
