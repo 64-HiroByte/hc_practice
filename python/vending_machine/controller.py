7f31b9ccffc4757a84619c697fb9bff705e6a0d7
64-HiroByte <higher.ground.rc@gmail.com>
2024-07-07 22:37:40 +0900

    Docstringの記述、controller.pyにステータス表示する機能を追加で実装した

--- python/vending_machine/controller.py
+++ python/vending_machine/controller.py
@@ -7,14 +7,29 @@ from my_exceptions import NoStockError
 from my_exceptions import SmallDepositError
 
 
-def get_balance_msg():
+def get_balance_msg(suica):
+    """Suicaの残高を表示する文字列の取得
+
+    Args:
+        suica (object): Suicaクラスをインスタンス化したオブジェクト
+
+    Returns:
+        str: 現在のSuicaの残高を記述したもの
+    """    
     return f'現在のSuicaの残高は {suica.balance}円 です\n'
 
 
-def charge_to_suica(sep_line='', quit='q'):
+def charge_to_suica(suica, sep_line='', quit='q'):
+    """Suicaにチャージ（入金）する処理
+
+    Args:
+        suica (object): Suicaクラスをインスタンス化したオブジェクト
+        sep_line (str, optional): 仕切り線（初期値 ''）
+        quit (str, optional): 前の画面に戻る時に入力するコマンド（初期値 'q'）
+    """    
     while True:
-        # 現在のSuicaの残高を取得
-        view.show_message(sep_line, get_balance_msg())
+        # 現在のSuicaの残高を取得し表示
+        view.show_message(sep_line, get_balance_msg(suica))
         
         # 入金額を入力
         input_value = view.input_deposit(suica.min_deposit)
@@ -30,31 +45,54 @@ def charge_to_suica(sep_line='', quit='q'):
             print(e)
             continue
         
+        # 入金完了処理
         additional_msg = f'Suicaに {input_value}円 チャージしました\n'
-        view.show_message(sep_line, get_balance_msg(), additional_msg)
+        view.show_message(sep_line, get_balance_msg(suica), additional_msg)
         break
 
 
-def get_stock_options(juice_lists, stock_nums, purchase=True):
+def get_stock_options(suica=None, vm=None, purchase=True):
+    """ジュースの名前と価格、ステータスを格納した選択肢を作成
+
+    Args:
+        suica (object, optional): Suicaクラスをインスタンス化したオブジェクト（初期値 None）
+        vm (object, optional): VendingMachineクラスをインスタンス化したオブジェクト（初期値 None）
+        purchase (bool, optional): 購入処理フラグ（初期値 True）
+
+    Returns:
+        list: ジュースの名前と価格、ステータス（在庫本数など）を格納したリスト
+    """
+    juice_lists = vm.juice_lists
+    stock_nums = vm.get_stock_nums()
+    
+    # 選択肢の作成
     stock_options = []
     for i, juice_list, in enumerate(juice_lists):
-        status = f'残り {stock_nums[i]}本'
+        status = f': 残り {stock_nums[i]}本'
         
-        if purchase:
+        if purchase:  #　購入の場合
             price = f'（{juice_list[1]}円）'
             if stock_nums[i] == 0:
-                status = '売り切れ'
+                status = ': 売り切れ'
             elif juice_list[1] > suica.balance:
-                status = '残高不足'
-        else:
+                status = ': 残高不足'
+        else:  # 補充の場合
             price = ''
-        option = f'{juice_list[0]}{price}    {status}'
+        option = f'{juice_list[0]}{price}{status}'
         stock_options.append(option)
     return stock_options
 
 
-# ジュースの購入操作
-def purchase_juice(juice_lists, sep_line='', quit='q'):
+def purchase_juice(suica, vm, sep_line='', quit='q'):
+    """ジュースを購入する
+
+    Args:
+        suica (object, optional): Suicaクラスをインスタンス化したオブジェクト（初期値 None）
+        vm (object, optional): VendingMachineクラスをインスタンス化したオブジェクト（初期値 None）
+        sep_line (str, optional): 仕切り線（初期値 ''）
+        quit (str, optional): 前の画面に戻る時に入力するコマンド（初期値 'q'）
+    """
+    juice_lists = vm.juice_lists
     # 自販機で扱うジュースの最低価格を取得
     min_price = min([juice[1] for juice in juice_lists])
     
@@ -75,50 +113,60 @@ def purchase_juice(juice_lists, sep_line='', quit='q'):
         else:
             additional_msg = ''
         
-        # Suica残高表示
-        view.show_message(sep_line, get_balance_msg(), additional_msg)
+        # Suicaの残高表示
+        view.show_message(sep_line, get_balance_msg(suica), additional_msg)
         if additional_msg:
             break
         
         # 購入処理
-        stock_options = get_stock_options(vm.juice_lists, vm.get_stock_nums())
+        stock_options = get_stock_options(suica, vm)
         msg = '購入したいジュースの番号を選択してください\n'
-        selected_option = view.get_selected_option(stock_options, msg, sep_line)
+        selected_option = view.input_selected_option(stock_options, msg, sep_line)
         
         if selected_option == quit:
             break
         
         i = int(selected_option)
-        # 購入商品と残高の比較
+        # バリデーションチェック （購入商品と残高の比較）
         try:
             vm.is_purchasable(i, suica.balance)
         except InsufficientBalanceError as e:
             print(e)
             continue
-        # 購入商品の在庫確認
+        # バリデーションチェック （購入商品の在庫確認）
         try:
             vm.is_in_stock(i)
         except NoStockError as e:
             print(e)
             continue
-        
+
+        # 自動販売機の在庫を1減らし、売上金の加算、Suicaから支払いを実行
         purchased_juice = vm.stocks[i].pop(0)
         amount = purchased_juice.price
         vm.add_proceeds(amount)
         suica.add_deposit(amount, deposit=False)
         
+        # 購入処理完了
         purchased_msg = f'{purchased_juice.name}({amount}円)を購入しました。Suicaの残高は{suica.balance}円です'
         view.show_message(sep_line, purchased_msg)
-        txt = '続けて購入しますか？'
-        res = view.input_yes_or_no(txt)
+        
+        # 反復処理の確認
+        res = view.input_yes_or_no('続けて購入しますか？')
         if res == 'n':
             break
         else:
             continue
 
 
-# ジュースの補充: whileループ
-def restock_juice(juice_lists, sep_line='', quit='q'):
+def restock_juice(vm, sep_line='', quit='q'):
+    """自動販売機にジュースを補充する
+
+    Args:
+        vm (object, optional): VendingMachineクラスをインスタンス化したオブジェクト
+        sep_line (str, optional): 仕切り線（初期値 ''）
+        quit (str, optional): 前の画面に戻る時に入力するコマンド（初期値 'q'）
+    """
+    juice_lists =vm.juice_lists
     msg = [
         '補充するジュースの番号を選択してください',
         f'補充する本数を入力してください（戻る場合は "{quit}" を入力） > ',
@@ -126,61 +174,101 @@ def restock_juice(juice_lists, sep_line='', quit='q'):
         ]
     
     while True:
-    # 在庫状況の取得
-        stock_options = get_stock_options(juice_lists, vm.get_stock_nums(), purchase=False)
-        selected_option = view.get_selected_option(stock_options, msg[0], sep_line, quit)
+        # 在庫状況の取得と選択
+        stock_options = get_stock_options(vm=vm, purchase=False)
+        selected_option = view.input_selected_option(stock_options, msg[0], sep_line, quit)
         if selected_option == quit:
             break
-        else:
-            i = int(selected_option)
+        
+        # 補充するジュースのインデックス番号
+        i = int(selected_option)
+        
+        # 補充する本数の入力
         num = view.input_value_validation(msg[1], quit)
         if num == quit:
             break
+
+        # ジュースの補充
         vm.restock(i, num)
+        
+        # 補充処理完了
         restocked_msg = f'{juice_lists[i][0]}を{num}本補充しました'
         view.show_message(sep_line, restocked_msg)
+        
+        # 反復処理の確認
         res = view.input_yes_or_no(msg[2])
         if res == 'n':
                 break
         else:
-            continue 
+            continue
+        
+        
+def show_statuses(suica, vm, sep_line=''):
+    """各種ステータスを表示させる
+
+    Args:
+        ssuica (object, optional): Suicaクラスをインスタンス化したオブジェクト
+        vm (object, optional): VendingMachineクラスをインスタンス化したオブジェクト
+        sep_line (str, optional): 仕切り線（初期値 ''）
+    """
+    juice_lists = vm.juice_lists
+    stock_nums = vm.get_stock_nums()
+    proceeds = vm.proceeds
+    
+    msg_lists = [get_balance_msg(suica), '** 自動販売機の在庫状況 **']
+    for juice_list, stock_num in zip(juice_lists, stock_nums):
+        msg_list = f'- {juice_list[0]}（{juice_list[1]}円）: 在庫{stock_num}本'
+        msg_lists.append(msg_list)
+    
+    msg_lists.append(f'\n現在の自動販売機の売上金額は {proceeds}円です')
+    
+    msg = '\n'.join(msg_lists)
+    
+    view.show_message(sep_line, msg)
 
 # mode選択
 def mode_select(options, sep_line):
-    msg = '何をしますか？'
-    selected_option = view.get_selected_option(options, msg, sep_line)
+    """行動を選択する
+
+    Args:
+        options (tuple): 行動の選択肢
+        sep_line (str, optional): 仕切り線（初期値 ''）
+
+    Returns:
+        str: 標準入力によって得た選択肢の番号（文字列）
+    """    
+    msg = '\n何をしますか？\n'
+    selected_option = view.input_selected_option(options, msg, sep_line)
     
     return selected_option
 
 
 def main():
-    while True:
-        juice_lists = vm.juice_lists
-        mode = mode_select(MODE_OPTIONS, sep_line)
-
-        # [0]: Suicaにチャージ
-        if mode == '0':
-            charge_to_suica(sep_line)
-        # [1]: ジュースの購入
-        if mode == '1':
-            purchase_juice(juice_lists, sep_line)
-        # [2]: ジュースの補充
-        if mode == '2':
-            restock_juice(juice_lists, sep_line)
-        # [q]: ループ処理の終了
-        if mode == 'q':
-            break
-
-
-if __name__ == '__main__':
     DEFAULT_DEPOSIT = 500
     MIN_DEPOSIT = 100
     JUICE_LISTS = [('ペプシ', 150), ('モンスター', 230), ('いろはす', 120)]
-    DEFAULT_NUM = 1
-    MODE_OPTIONS = ('Suicaにチャージ', 'ジュースの購入', 'ジュースの補充')
+    DEFAULT_NUM = 5
+    MODE_OPTIONS = ('Suicaにチャージ', 'ジュースの購入', 'ジュースの補充', 'ステータス表示')
 
     # 初期設定値 -- 変数
     sep_line = '=' * 70 + '\n'
     suica = Suica(DEFAULT_DEPOSIT, MIN_DEPOSIT)
     vm = VendingMachine(JUICE_LISTS, DEFAULT_NUM)
-    main()
+    
+    while True:
+        mode = mode_select(MODE_OPTIONS, sep_line)
+
+        if mode == '0':  # Suicaにチャージ
+            charge_to_suica(suica, sep_line)
+        
+        if mode == '1':  # ジュースの購入
+            purchase_juice(suica, vm, sep_line)
+        
+        if mode == '2':  # ジュースの補充
+            restock_juice(vm, sep_line)
+        
+        if mode == '3':  # ジュースの補充
+            show_statuses(suica, vm, sep_line)
+
+        if mode == 'q':  # ループ処理の終了
+            break
--- python/vending_machine/view.py
+++ python/vending_machine/view.py
@@ -1,10 +1,28 @@
-# 仕切り線付きの標準出力
 def show_message(sep_line='', main_msg='', additional_msg=''):
+    """メッセージを標準出力する
+
+    Args:
+        sep_line (str, optional): 仕切り線（初期値 ''）
+        main_msg (str, optional): メインのメッセージ（初期値 ''）
+        additional_msg (str, optional): 追加のメッセージ（初期値 ''）
+    """    
     print(f'{sep_line}\n{additional_msg}{main_msg}')
 
 
 # input()のバリデーションチェック int型か正の値か判定　
 def input_value_validation(txt, quit='q'):
+    """標準入力された値のバリデーションチェックと取得
+
+    Args:
+        txt (str): 標準入力時に表示させる文字列
+        quit (str, optional): 前の画面に戻る時に入力するコマンド（初期値 'q'）
+
+    Raises:
+        ValueError: intに型変換できない、または負の値が入力された場合（quitコマンドを除く）
+
+    Returns:
+        int: 正の整数
+    """    
     while True:
         input_value = input(txt)
         if input_value == quit:
@@ -23,20 +41,41 @@ def input_value_validation(txt, quit='q'):
 
 # Suicaに入金する金額を入力するための関数
 def input_deposit(min_deposit, quit='q'):
+    """Suicaにチャージする金額の入力
+
+    Args:
+        min_deposit(int): Suicaへの最少入金額
+        quit (str, optional): 前の画面に戻る時に入力するコマンド（初期値 'q'）
+
+    Returns:
+        int: Suicaにチャージする金額（正の値）
+    """
     txt = f'Suicaにチャージする金額を入力してください\n（最低チャージ額: {min_deposit}円, 前に戻る: "{quit}"） > '
     return input_value_validation(txt)
 
 
 # 標準入力した値が選択肢に含まれているか判定する関数
-def get_selected_option(options, msg, sep_line='', quit='q'):
+def input_selected_option(options, msg, sep_line='', quit='q'):
+    """選択肢から任意のインデックス番号を選択する
+
+    Args:
+        options (list): 選択肢を格納したリスト
+        msg (str): 選択肢と一緒に表示するメッセージ
+        sep_line (str, optional): 仕切り線（初期値 ''）
+        quit (str, optional): 前の画面に戻る時に入力するコマンド（初期値 'q'）
+        
+    Returns:
+        str: 選択した選択肢のインデックス番号（文字列）またはquitコマンド
+    """
     selectable_num = [quit]
     show_options = sep_line + msg
     
+    # 選択肢のインデックス番号と選択肢を表示する文字列
     for i, option in enumerate(options):
         selectable_num.append(str(i))
         show_options += f'\n{i} : {option}'
-    # print(f'ここでは {" / ".join(options)} を選択できます')
-
+    
+    # quitコマンドまたは選択肢のインデックス番号に合致するまでループ
     while True:
         print(show_options)
         selected_option = input(f'\n番号を入力してください（戻る場合は "{quit}" を入力） > ')
@@ -49,6 +88,14 @@ def get_selected_option(options, msg, sep_line='', quit='q'):
 
 
 def input_yes_or_no(txt):
+    """反復処理を実行するか確認する
+
+    Args:
+        txt (str): 反復処理の確認メッセージ
+
+    Returns:
+        str: 'y'または'n'
+    """    
     result = ''
     while result != 'y' and result != 'n':
         result = input(txt + '（y/n） > ').lower()
